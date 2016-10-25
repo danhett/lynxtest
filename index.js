0adaae40cb6178d89f35230958ed8d8cd1cbd019
hellodanhett@gmail.com <dan.hett@havaslynx.com>
2016-10-25 15:20:30 +0100

    Updated with demo files

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-var express = require('express')
-var app = express()
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
 
-app.get('/', function(request, response) {
-  response.send('Adam smells!')
-})
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
