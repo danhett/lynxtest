e65e195cd4e3a3e7ca6290a1fa14a4e3d5999295
hellodanhett@gmail.com <dan.hett@havaslynx.com>
2016-10-25 15:31:37 +0100

    Added Path dependency

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var path = require('path');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
